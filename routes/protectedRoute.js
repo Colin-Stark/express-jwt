7b0de4d5afbbced2b46409d4d793c231551187e6
Colin-Stark <collinscodes@gmail.com>
2026-01-05 14:09:12 -0500

    Add express-rate-limit middleware for rate limiting on protected routes

--- routes/protectedRoute.js
+++ routes/protectedRoute.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const authenticateToken = require('../middleware/authenticateToken');
+const rateLimiter = require('../middleware/rateLimit');
+
+// Apply rate limiting middleware to all routes in this router
+// router.use(rateLimiter);
 
 
-router.get('/', authenticateToken, (req, res) => {
+router.get('/', authenticateToken, rateLimiter, (req, res) => {
     res.status(200).json(
         {
             message: `Welcome ${req.user.username}, you have accessed a protected route!`
